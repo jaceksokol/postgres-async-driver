ce733fbd01b1ca85e7f41b6b558e5ff3307012c2
Jacek Sokol <Jacek.Sokol@uk.tesco.com>
2017-12-01 21:22:03 +0100

    Try to fix build

--- src/test/java/com/github/pgasync/impl/DatabaseRule.java
+++ src/test/java/com/github/pgasync/impl/DatabaseRule.java
@@ -11,6 +11,7 @@ import com.github.pgasync.ResultSet;
 import org.junit.rules.ExternalResource;
 import rx.Observable;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.List;
@@ -39,23 +40,24 @@ class DatabaseRule extends ExternalResource {
 
     DatabaseRule(ConnectionPoolBuilder builder) {
         this.builder = builder;
-        if (builder instanceof EmbeddedConnectionPoolBuilder)
-        {
-            if (process == null)
-            {
-                try
-                {
+        if (builder instanceof EmbeddedConnectionPoolBuilder) {
+            if (process == null) {
+                try {
+                    File dir = new File(getClass().getClassLoader().getResource(".").getPath(), "postgres-data");
+                    dir.mkdir();
                     PostgresStarter<PostgresExecutable, PostgresProcess> runtime = PostgresStarter.getDefaultInstance();
-                    PostgresConfig config = new PostgresConfig(V9_6_2, new AbstractPostgresConfig.Net(),
-                        new AbstractPostgresConfig.Storage("async-pg"), new AbstractPostgresConfig.Timeout(),
-                        new AbstractPostgresConfig.Credentials("async-pg", "async-pg"));
+                    PostgresConfig config = new PostgresConfig(
+                            V9_6_2,
+                            new AbstractPostgresConfig.Net(),
+                            new AbstractPostgresConfig.Storage("async-pg", dir.getAbsolutePath()),
+                            new AbstractPostgresConfig.Timeout(),
+                            new AbstractPostgresConfig.Credentials("async-pg", "async-pg")
+                    );
                     PostgresExecutable exec = runtime.prepare(config);
                     process = exec.start();
 
                     out.printf("Started postgres to %s:%d%n", process.getConfig().net().host(), process.getConfig().net().port());
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             }
@@ -67,14 +69,14 @@ class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        if(pool == null) {
+        if (pool == null) {
             pool = builder.build();
         }
     }
 
     @Override
     protected void after() {
-        if(pool != null) {
+        if (pool != null) {
             try {
                 pool.close().await();
             } catch (Exception e) {
@@ -86,24 +88,25 @@ class DatabaseRule extends ExternalResource {
     ResultSet query(String sql) {
         return block(db().querySet(sql, (Object[]) null).toObservable());
     }
+
     @SuppressWarnings("rawtypes")
     ResultSet query(String sql, List/*<Object>*/ params) {
         return block(db().querySet(sql, params.toArray()).toObservable());
     }
 
     private <T> T block(Observable<T> observable) {
-        BlockingQueue<Entry<T,Throwable>> result = new ArrayBlockingQueue<>(1);
+        BlockingQueue<Entry<T, Throwable>> result = new ArrayBlockingQueue<>(1);
         observable.single().subscribe(
                 item -> result.add(new SimpleImmutableEntry<>(item, null)),
                 exception -> result.add(new SimpleImmutableEntry<>(null, exception)));
         try {
 
-            Entry<T,Throwable> entry = result.poll(5, TimeUnit.SECONDS);
-            if(entry == null) {
+            Entry<T, Throwable> entry = result.poll(5, TimeUnit.SECONDS);
+            if (entry == null) {
                 throw new RuntimeException("Timed out waiting for result");
             }
             Throwable exception = entry.getValue();
-            if(exception != null) {
+            if (exception != null) {
                 throw exception instanceof RuntimeException
                         ? (RuntimeException) exception
                         : new RuntimeException(exception);
